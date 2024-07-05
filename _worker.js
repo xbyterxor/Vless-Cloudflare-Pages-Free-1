8f69ecfaf2e9e4f5c5c4ed8caff7f87cc542b6cf
xbyterxor <157615428+xbyterxor@users.noreply.github.com>
2024-07-05 16:10:11 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '652da35d-b59d-4ab0-af59-cb90066dcc10';
+let userID = '1c1d838f-053b-4081-aa88-75d6ff83eb7f';
 
 const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
